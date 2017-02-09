96558291bcb3cb0d59027506b3cf32923d632b97
alexanderruzhev@gmail.com <alexanderruzhev@gmail.com>
2017-02-09 14:50:14 +0300

    added hello world

---
+++ src/ru/job4j/Calculate.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+/**
+* Class output in the console is "Hello world!"
+* @author aruzhev
+* @since 08.02.2017
+* @version 1
+*/
+public class Calculate {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
