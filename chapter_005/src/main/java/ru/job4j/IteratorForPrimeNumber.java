6f53b239a9ba9986ed961ab74c14114e88807dd0
wolfdog007 <alexanderruzhev@gmail.com>
2017-10-02 15:18:18 +0300

    2.005.1.3 Создать итерато простые числа.[#151]

---
+++ chapter_005/src/main/java/ru/job4j/IteratorForPrimeNumber.java
@@ -0,0 +1,84 @@
+package ru.job4j;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author Ruzhev Alexander
+ * @since 02.10.2017
+ */
+public class IteratorForPrimeNumber implements Iterator {
+    /**
+     * Array is int.
+     */
+    private int[] array;
+    /**
+     * The position of the iterator.
+     */
+    private int position = 0;
+
+    /**
+     * Constructor.
+     *
+     * @param array is int.
+     */
+    public IteratorForPrimeNumber(final int[] array) {
+        this.array = array;
+    }
+
+    /**
+     * Check number for simplicity.
+     *
+     * @param number Number to test
+     * @return true for prime number
+     */
+    private boolean checkPrime(int number) {
+        boolean result = true;
+        if (number < 1) {
+            result = false;
+        }
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns {@code true} if the iteration has more elements.
+     * (In other words, returns {@code true} if {@link #next} would
+     * return an element rather than throwing an exception.)
+     *
+     * @return {@code true} if the iteration has more elements
+     */
+    @Override
+    public boolean hasNext() {
+        boolean result = false;
+        for (int i = this.position; i < this.array.length; i++) {
+            if (this.checkPrime(this.array[i])) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns the next element in the iteration.
+     *
+     * @return the next element in the iteration
+     * @throws NoSuchElementException if the iteration has no more elements
+     */
+    @Override
+    public Object next() {
+        try {
+            while (!this.checkPrime(this.array[this.position])) {
+                this.position++;
+            }
+            return this.array[this.position++];
+        } catch (Exception ex) {
+            throw new NoSuchElementException();
+        }
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/IteratorForPrimeNumberTest.java
@@ -0,0 +1,56 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Ruzhev Alexander
+ * @since 02.10.2017
+ */
+public class IteratorForPrimeNumberTest {
+    /**
+     * Test hasNext().
+     */
+    @Test
+    public void whenHasNext() {
+        int[] array = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
+        IteratorForPrimeNumber iterator = new IteratorForPrimeNumber(array);
+        assertThat(iterator.hasNext(), is(true));
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        assertThat(iterator.hasNext(), is(false));
+    }
+
+    /**
+     * Test next().
+     */
+    @Test
+    public void whenNextThenGetPrimeNumber() {
+        int[] array = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
+        IteratorForPrimeNumber iterator = new IteratorForPrimeNumber(array);
+        assertThat(iterator.next(), is(1));
+        assertThat(iterator.next(), is(2));
+        assertThat(iterator.next(), is(3));
+        assertThat(iterator.next(), is(5));
+        assertThat(iterator.next(), is(7));
+    }
+
+    /**
+     * Test next() then NoSuchElementException.
+     *
+     * @throws NoSuchElementException when element not found.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void whenNextThenNoSuchElementException() throws NoSuchElementException {
+        int[] array = new int[]{0, 4, 6, 8};
+        IteratorForPrimeNumber iterator = new IteratorForPrimeNumber(array);
+        iterator.next();
+    }
+}
