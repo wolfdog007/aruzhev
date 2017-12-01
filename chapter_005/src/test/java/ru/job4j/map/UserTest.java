73b18f01e0a1f3406166e940741b1996a8992a7e
wolfdog007 <alexanderruzhev@gmail.com>
2017-12-01 14:48:41 +0300

    2.5.5.2 Не перекрывать equals hashCode [#1005]

---
+++ chapter_005/src/test/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test for UserTest.
+ *
+ * @author Ruzhev Alexander
+ * @since 30.11.2017
+ */
+public class UserTest {
+    /**
+     * The test method for check result output.
+     */
+    @Test
+    public void whenCheckOutputResult() {
+        GregorianCalendar date = new GregorianCalendar(1983, 3, 15);
+        User user1 = new User("Alexandr", 0, date);
+        User user2 = new User("Alexandr", 0, date);
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, "one");
+        map.put(user2, "two");
+        System.out.print(map);
+        assertThat(user1.getName().equals(user2.getName()), is(true));
+        assertThat(user1.getChildren() == user2.getChildren(), is(true));
+        assertThat(user1.getBirthday().equals(user2.getBirthday()), is(true));
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/map/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Ruzhev Alexander
+ * @since 01.12.2017
+ */
+package ru.job4j.map;
